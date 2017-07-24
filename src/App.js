8dfc0a11f2c30be3459786b4bbddd37bfaa5fef3
Jordan Levine & Jae Easley <pair+j0rdania.jaesley@devbootcamp.com>
2017-07-24 16:58:37 -0700

    Create Login component stub

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to MyBooks</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Login />
       </div>
     );
+  }
+}
+
+class Login extends Component {
+  render() {
+    return <p>Login here</p>
   }
 }
 
