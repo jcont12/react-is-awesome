3bb9d33384de96515e8e0541f13f5f6f455da6aa
Cassi Gallagher & Jeremy Lee <pair+cassig.pyrofist@devbootcamp.com>
2017-07-24 15:29:31 -0700

    Get all foods and display them to page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { foods: null };
+  }
+  componentDidMount() {
+    $.get('http://localhost:3000/foods', (data) => {
+      this.setState({ foods: data });
+    });
+  }
   render() {
+    const foodItems = this.state.foods ? this.state.foods.map((food, index) => <div key={index}>{food.name}</div>) : 'No Food Items';
+
     return (
-      <h1>FOODZ</h1>
+      <div>
+        <h1>FOODZ</h1>
+        <div>{foodItems}</div>
+      </div>
     );
   }
+
 }
 
 export default App;
