d8c000b79dc6e62f39de79c9b549f9a9c24c1ec2
Jordan Levine & Jae Easley <pair+j0rdania.jaesley@devbootcamp.com>
2017-07-25 15:23:21 -0700

    Create BookList component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BookList from './BookList.js'
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: null,
-    }
-  }
+
+  // logIn() {
+  //   let name = 'Anil'
+  //   let loginHandler = (username, password) => {
+  //     $.ajax = {
+  //       method: 'post',
+  //       url: 'http://localhost:3000/sessions',
+  //       data: this.serialize(),
+  //     }
+  //   }
+  // }
+
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to MyBooks</h2>
+          <BookList />
         </div>
-        <Login />
       </div>
     );
   }
 }
 
-class Login extends Component {
-  render() {
-    return (
-      <p>
-        <form id='login-form' method='post' action='http://localhost:3000/sessions'>
-          <input type='text' name='user[name]' placeholder='Name' />
-          <input type='password' name='user[password]' placeholder='Password' />
-          <input type='submit' name='login' value='Log In'
-            onClick={props.onClick}/>
-        </form>
-      </p>
-      // {"user_id":431,"token":"eyJ0b2tlbiI6NDMxfQ==\n"}
-    );
-  }
-}
+// class Login extends Component {
+//   handleClick() {
+//     console.log('in login component');
+//     console.log('this to follow');
+//     console.log(this);
+//   }
+//
+//   render() {
+//     return (
+//         <form
+//           id='login-form'
+//           method='post'
+//           action='http://localhost:3000/sessions'
+//           onSubmit={this.handleClick} >
+//           <input type='text' name='user[name]' placeholder='Name' />
+//           <input type='password' name='user[password]' placeholder='Password' />
+//           <input type='submit' name='login' value='Log In'/>
+//         </form>
+//       // {"user_id":431,"token":"eyJ0b2tlbiI6NDMxfQ==\n"}
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/BookList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        THIS IS JAE's and SPARKY's BOOKLIST.
+      </div>
+    );
+  }
+}
+export default BookList;
