0d72de8cfbf3cb421d81cbb4ff142da8e341ded3
Cassi Gallagher & Jeremy Lee <pair+cassig.pyrofist@devbootcamp.com>
2017-07-25 12:32:07 -0700

    Add delete book item from list as added to reading list

--- src/App.js
+++ src/App.js
@@ -10,25 +10,35 @@ class App extends Component {
     this.state = {
       books: [],
       selectedBooks: [],
+      readingList: [],
     };
   }
   componentDidMount() {
+    this.getAllBooks();
+  }
+
+  getAllBooks() {
     $.get('http://localhost:3000/books', (data) => {
       this.setState({ books: data });
     });
   }
 
   render() {
-    const bookList = [];
-
     const selectBook = (book) => {
-      console.log(book);
+      const newBookList = this.state.readingList.slice();
+      if (!newBookList.includes(book)) {
+        newBookList.push(book);
+        const newBooks = this.state.books.filter(filterBook =>
+          filterBook !== book,
+        );
+        this.setState({ readingList: newBookList, books: newBooks });
+      }
     };
     return (
       <div>
         <h1>BOOKZ</h1>
         <h2>Reading List</h2>
-        <div>{this.state.books ? <ReadingList books={bookList} /> : ''}</div>
+        <div>{this.state.books ? <ReadingList books={this.state.readingList} /> : ''}</div>
         <h2>All Books</h2>
         <SelectableBookList books={this.state.books} selectBook={selectBook} />
       </div>
