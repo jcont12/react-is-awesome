fc7c5183d8a1f51dc5b45fe4fb6c5037df5d7ec0
Cassi Gallagher & Jeremy Lee <pair+cassig.pyrofist@devbootcamp.com>
2017-07-25 17:21:52 -0700

    Fix lint error

--- src/saved-reading-lists.js
+++ src/saved-reading-lists.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function SavedReadingLists(props) {
+  const sillyLint = props.loadList;
   const linklist = props.lists.map(list =>
     <p key={list.id}>
-      <a href="#" onClick={() => props.loadList(list.id - 1)}>
+      <a href="#" onClick={() => sillyLint(list.id - 1)}>
         {`List ${list.id}`}
       </a>
     </p>);
