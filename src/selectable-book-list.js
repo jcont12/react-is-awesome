003971a580d24fdca0faa0c315e4074113bca17d
Cassi Gallagher & Jeremy Lee <pair+cassig.pyrofist@devbootcamp.com>
2017-07-25 17:29:26 -0700

    Convert SelectableBookList to pure component

--- src/selectable-book-list.js
+++ src/selectable-book-list.js
@@ -3,32 +3,23 @@ import PropTypes from 'prop-types';
 import './App.css';
 import Book from './book';
 
-class SelectableBookList extends React.Component {
-  renderBook(i) {
-    return (<Book
-      key={this.props.books[i].id}
-      title={this.props.books[i].title}
-      author={this.props.books[i].author}
-    />);
-  }
-
-  render() {
-    const bookItems = this.props.books.length > 0 ?
-      this.props.books.map((book) => {
-        const chooseBook = () => { this.props.selectBook(book); };
-        return (
-          <div key={book.id}>
-            <button onClick={chooseBook}>Add to List</button>
-            <Book
-              title={book.title}
-              author={book.author}
-            />
-          </div>
-        );
-      }) :
-      'Loading books....';
-    return <div>{bookItems}</div>;
-  }
+function SelectableBookList(props) {
+  const selectBook = props.selectBook;
+  const bookItems = props.books.length > 0 ?
+    props.books.map((book) => {
+      const chooseBook = () => { selectBook(book); };
+      return (
+        <div key={book.id}>
+          <button onClick={chooseBook}>Add to List</button>
+          <Book
+            title={book.title}
+            author={book.author}
+          />
+        </div>
+      );
+    }) :
+    'Loading books....';
+  return <div>{bookItems}</div>;
 }
 
 SelectableBookList.propTypes = {
